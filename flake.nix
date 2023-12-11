a12180ce5c9101dc25a26221193aa23c74825887
Jonathan Hope <jhope@theflatfield.net>
2023-12-10 22:30:59 -0800

    feat: macos builds (#15)

--- flake.nix
+++ flake.nix
@@ -6,30 +6,31 @@
       flake = false;
     };
     zig.url = "github:mitchellh/zig-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-compat, zig }:
-    let pkgs = import nixpkgs {
-        system = "x86_64-linux";
-    };
-    in
-    {
-      devShell.x86_64-linux =
-        pkgs.mkShell {
+  outputs = { self, nixpkgs, flake-compat, flake-utils, zig }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
           buildInputs = with pkgs;[
             go_1_21
             gopls
             go-task
             golangci-lint
             goose
-            zig.packages.${system}.master
+            # https://github.com/ziglang/zig/issues/17130
+            zig.packages.${system}.master-2023-08-15
             goreleaser
             syft
-            snyk
+            # snyk
           ];
           shellHook = ''
             export PATH=$PWD/tools:$PATH
           '';
         };
-    };
+      }
+    );
 }
