1229c22f00a9a0f69c7b1490dee8065b99b258d4
Jonathan Hope <jhope@fastmail.com>
2024-01-09 09:23:48 -0800

    feat: added a medium seeder

--- cmd/cli/tui/msgs/books.go
+++ cmd/cli/tui/msgs/books.go
@@ -2,3 +2,6 @@ package msgs
 
 // FolderMsg is a message that changes the current folder.
 type FolderMsg string
+
+// FreeMsg is a message that denotes the writer is free again.
+type FreeMsg struct{}
